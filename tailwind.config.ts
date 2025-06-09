78bfc70eb1c9e2c03a79d414a751ca9883542c92
jacques.y.b. <jacquesyann@live.fr>
2025-06-09 23:40:43 +0000

    add fivesixfivecolors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#c62f9d', //use bg-primary or text-primary to use these colors in components
+        'secondary': '#22a796', //use bg-secondary or text-secondary to use these colors in components
+        'neutral-dark': '#1C1C1C',
+        'neutral-light': '#F7F8FA',
+      }
+    },
   },
   plugins: [],
 } satisfies Config
