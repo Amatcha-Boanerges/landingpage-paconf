ebbc2b4ea72aa13122bbec1fe0403e5f91e6c13e
jacques.y.b. <jacquesyann@live.fr>
2025-06-10 00:09:09 +0000

    fix: lint Error: 'Image' is defined but never used

--- src/app/components/WhyAttend.tsx
+++ src/app/components/WhyAttend.tsx
@@ -1,38 +1,4 @@
 // app/_components/WhyAttend.tsx
-import Image from 'next/image';
-
-const reasons = [
-  {
-    image: '/images/why-attend-1.png',
-    text: 'Seeks to elevate your professional game of delivering outstanding results',
-    bgColor: 'bg-fuchsia-700',
-  },
-  {
-    image: '/images/why-attend-2.png',
-    text: 'Wants to broaden your scope of work beyond the borders of Ghana',
-    bgColor: 'bg-teal-600',
-  },
-  {
-    image: '/images/why-attend-3.png',
-    text: 'Is looking to equip yourself with insights, tools and relevant skills for the modern place of work',
-    bgColor: 'bg-fuchsia-700',
-  },
-  {
-    image: '/images/why-attend-4.png',
-    text: 'Seeks to transform your way of work to keep you relevant to your executives',
-    bgColor: 'bg-teal-600',
-  },
-  {
-    image: '/images/why-attend-5.png',
-    text: 'Seeks to add value to your professional and personal life',
-    bgColor: 'bg-fuchsia-700',
-  },
-  {
-    image: '/images/why-attend-6.png',
-    text: '',
-    bgColor: '',
-  },
-];
 
 export default function WhyAttend() {
   return (
