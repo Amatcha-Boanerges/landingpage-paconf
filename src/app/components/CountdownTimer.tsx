c97f1eb5366b526f305f5847f98e1a32216040c9
Abdulaziz <31636180+guuree19@users.noreply.github.com>
2025-06-12 15:43:05 -0400

    countdown component file

---
+++ src/app/components/CountdownTimer.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React, {useEffect, useState  } from "react";
+
+ 
+export default function CountdownTimer() {
+    const [timeLeft, setTimeLeft] = useState({
+        days:0,
+        hours:0,
+        minutes:0,
+        seconds:0,
+    });
+    
+
+
+}
