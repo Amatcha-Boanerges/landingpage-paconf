aeae20535d0e1637f6d8c9fd140ab0ed5f2860cd
Ryan9056 <Wagner6@mail.uc.edu>
2025-06-13 12:20:18 -0400

    Fixed Hero File

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,4 @@
 // app/_components/Hero.tsx
-import Image from "next/image";
 
 export default function Hero() {
   return (
@@ -8,30 +7,7 @@ export default function Hero() {
         <div className="flex justify-center">
         </div>
         <h1 className="text-3xl md:text-4xl font-extrabold uppercase tracking-wide">
-                      <div className="pt-[25px] flex justify-center"> 
-    
-     
-            <Image 
-              src="/PA_conference_logo.png"
-              alt="Hero Image"
-              width={600}
-              height={233}
-              priority
-            />
-            </div>
-    
-            <div className="text-8xl leading-[100%] text-center"> 
-            GROWTH FOR CROSS BORDER REFERENCE
-            
-            <div className="pt-[50px] text-6xl text-[#C62F9D]">
-            17th - 18th JULY 2025
-            </div>
-
-            <div className="text-6xl text-[#22A696]">
-            LABADI BEACH HOTEL
-            </div>
-
-            </div>
+          Build your Hero section here
         </h1>
       </div>
     </section>
