89c094fe58986e310eeff9330e46928b40ce4abc
jacques.y.b. <jacquesyann@live.fr>
2025-06-13 22:33:52 +0000

    fix wrong primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#061d49', //use bg-primary or text-primary to use these colors in components
+        'primary': '#c62f9d', //use bg-primary or text-primary to use these colors in components
         'secondary': '#22a796', //use bg-secondary or text-secondary to use these colors in components
         'pa-background': '#05183E',
         'neutral-dark': '#1C1C1C',
