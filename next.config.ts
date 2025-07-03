14a012d7ec7d6df35f9a9f8231befed7dbb727c6
DevonAGreene <greened8@mail.uc.edu>
2025-07-03 14:44:11 -0400

    Edited next.config.ts file to enforce HTTPS redirects (Phase 2 - End) ('destination' will be replaced with deployed domain when avaliable)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'yourdomain.com',  // replace with your actual domain
+          },
+        ],
+        destination: 'https://yourdomain.com/:path*', // replace with your domain too
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
