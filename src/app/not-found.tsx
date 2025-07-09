9837c33a8d98f0eed51f1909b530e8ce258e1e76
Abdulaziz <guuree1982@gmail.com>
2025-07-09 13:00:56 -0400

    chore: ensure clean install and build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,8 @@ export default function NotFound() {
     <div style={{ textAlign: 'center', padding: '2rem' }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+      <Link href="/" 
+        style={{ color: 'blue', textDecoration: 'underline' }}>
         Go back home
       </Link>
     </div>
