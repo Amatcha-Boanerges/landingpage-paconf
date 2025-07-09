137be330c5542bd9d8dde40ac3b27364aa0b96c7
Ryan9056 <Wagner6@mail.uc.edu>
2025-07-09 17:13:49 -0400

    Update for ci tests 2

--- src/app/(site)/auth/change-password/page.tsx
+++ src/app/(site)/auth/change-password/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/lib/supabase/server'
-import  ChangePasswordFrom  from '@/app/components/auth/change-password-form'
 import ChangePasswordForm from '@/app/components/auth/change-password-form';
 
 // Force dynamic rendering to prevent static generation issues
